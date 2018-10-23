c4f3a5713aff384858dab8bc3d9a05e5292913fe
Level Up <techops@Levels-MacBook-Pro.local>
2018-10-23 20:32:07 +1100

    [Kinjal/Matty]SOSCall Nav added

--- client/spec/pages/SOSCall.spec.js
+++ client/spec/pages/SOSCall.spec.js
@@ -4,7 +4,27 @@ describe('SOSCall', () => {
     describe('#render', () => {
         it('should render SOS Call', () => {
             const page = new SOSCall();
-            expect(page.render()).toContain("<div>SOSCall</div>");
-        });
+            expect(page.render())
+            .toContain(
+                `<div class=\"container\">
+    <div>Cancel</div>
+    <div>Calling Police...</div>
+    <div></div>
+</div>`)
+        }); //do not touch formatting haha
     });
+    describe('#topButtonEvent', () => {
+        it('goes to Home page', () => {
+            const props = {
+                navigate: () => { },
+            };
+    
+          const page = new SOSCall(props);
+          spyOn(page, 'navigate');
+    
+          page.topButtonEvent();
+          expect(page.navigate).toHaveBeenCalledWith('/');
+    
+        });
+      });
 });
--- client/src/js/pages/SOSCall.js
+++ client/src/js/pages/SOSCall.js
@@ -2,6 +2,10 @@ const BasePage = require('watch-framework').BasePage;
 
 class SOSCall extends BasePage {
     template = require('../../templates/SOSCall.hbs');
-}
 
+topButtonEvent() {
+    this.navigate('/');
+  }
+
+}
 module.exports = SOSCall;
