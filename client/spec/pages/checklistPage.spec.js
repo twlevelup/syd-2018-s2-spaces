4f2e3bd1aa262d4c2b79296a03f26970defe8c04
Daniel Ting <dting@outlook.com>
2018-10-27 10:38:58 +1100

    [Monica/Daniel] add style to checklist page

--- client/spec/pages/checklistPage.spec.js
+++ client/spec/pages/checklistPage.spec.js
@@ -7,9 +7,9 @@ describe('checklistPage', () => {
       const page = new checklistPage();
       page.pageWillLoad();
 
-      expect(page.render()).toContain("<div>Does someone beat you on a regular basis?</div>");
-      expect(page.render()).toContain("<div>Yes</div>");
-      expect(page.render()).toContain("<div>No</div>");
+      expect(page.render()).toContain("Does someone beat you on a regular basis?");
+      expect(page.render()).toContain("Yes");
+      expect(page.render()).toContain("No");
     });
   });
 
