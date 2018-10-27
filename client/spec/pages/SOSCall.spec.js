5e1b6bb5373fcd067454a756350f13636cd9c11d
Shuqian Hon (SQ) <honsq90@gmail.com>
2018-10-27 14:18:32 +1100

    [SQ/Connor] Add phone  icon to SOS call page.

--- client/spec/pages/SOSCall.spec.js
+++ client/spec/pages/SOSCall.spec.js
@@ -1,30 +1,24 @@
 const SOSCall = require('../../src/js/pages/SOSCall');
 
 describe('SOSCall', () => {
-    describe('#render', () => {
-        it('should render SOS Call', () => {
-            const page = new SOSCall();
-            expect(page.render())
-            .toContain(
-                `<div class=\"container\">
-    <div>Cancel</div>
-    <div>Calling Police...</div>
-    <div></div>
-</div>`)
-        }); //do not touch formatting haha
+  describe('#render', () => {
+    it('should render SOS Call', () => {
+      const page = new SOSCall();
+      expect(page.render()).toMatchSnapshot()
+    }); //do not touch formatting haha
+  });
+  describe('#topButtonEvent', () => {
+    it('goes to Home page', () => {
+      const props = {
+        navigate: () => { },
+      };
+
+      const page = new SOSCall(props);
+      spyOn(page, 'navigate');
+
+      page.topButtonEvent();
+      expect(page.navigate).toHaveBeenCalledWith('/');
+
     });
-    describe('#topButtonEvent', () => {
-        it('goes to Home page', () => {
-            const props = {
-                navigate: () => { },
-            };
-    
-          const page = new SOSCall(props);
-          spyOn(page, 'navigate');
-    
-          page.topButtonEvent();
-          expect(page.navigate).toHaveBeenCalledWith('/');
-    
-        });
-      });
+  });
 });
