9994c715522cfc9514097137ab817fca1345efe4
Shuqian Hon (SQ) <honsq90@gmail.com>
2018-10-27 14:30:12 +1100

    [SQ/Connor] Jump to SOS call after countdown finishes.

--- client/spec/pages/SOSPage.spec.js
+++ client/spec/pages/SOSPage.spec.js
@@ -1,42 +1,75 @@
 const SOSPage = require('../../src/js/pages/SOSPage');
 
 describe('SOSPage', () => {
-    describe('#render', () => {
-        it('should show SOS page', () => {
-            const page = new SOSPage();
-            expect(page.render()).toContain("YES");
-            expect(page.render()).toContain("Are you safe?");
-            expect(page.render()).toContain("NO");
-        });
+  describe('#render', () => {
+    it('should show SOS page', () => {
+      const page = new SOSPage();
+      expect(page.render()).toContain("YES");
+      expect(page.render()).toContain("Are you safe?");
+      expect(page.render()).toContain("NO");
     });
+  });
 
-    describe('#rightButtonEvent', () => {
-        it('goes to SOS Call', () => {
-            const props = {
-                navigate: () => { },
-            };
-    
-          const page = new SOSPage(props);
-          spyOn(page, 'navigate');
-    
-          page.rightButtonEvent();
-          expect(page.navigate).toHaveBeenCalledWith('SOSCall');
-    
-        });
-      });
+  describe('#pageWillLoad', () => {
+    let page;
+    const props = {
+      navigate: () => { },
+    };
+    beforeEach(() => {
+      jest.useFakeTimers();
+      page = new SOSPage(props);
+      spyOn(page, 'navigate');
 
-      describe('#leftButtonEvent', () => {
-        it('goes to Home page', () => {
-            const props = {
-                navigate: () => { },
-            };
-    
-          const page = new SOSPage(props);
-          spyOn(page, 'navigate');
-  
-          page.leftButtonEvent();
-          expect(page.navigate).toHaveBeenCalledWith('/');
-    
-        });
-      });
+    })
+
+    it('should initialise countdown as 10', () => {
+      page.pageWillLoad();
+      expect(page.countdown).toBe(10);
+    })
+
+    it('should countdown to 5 after 5 seconds', () => {
+      page.pageWillLoad();
+      jest.runTimersToTime(5000);
+      expect(page.countdown).toBe(5);
+    })
+
+    it('should countdown to 0 after 10 seconds', () => {
+      page.pageWillLoad();
+      jest.runTimersToTime(10000);
+      expect(page.countdown).toBe(0);
+      expect(page.navigate).toHaveBeenCalledWith('SOSCall');
+
+    })
+  })
+
+  describe('#rightButtonEvent', () => {
+    it('goes to SOS Call', () => {
+      const props = {
+        navigate: () => { },
+      };
+
+      const page = new SOSPage(props);
+      spyOn(page, 'navigate');
+
+      page.rightButtonEvent();
+
+      expect(page.navigate).toHaveBeenCalledWith('SOSCall');
+
+    });
+  });
+
+  describe('#leftButtonEvent', () => {
+    it('goes to Home page', () => {
+      const props = {
+        navigate: () => { },
+      };
+
+      const page = new SOSPage(props);
+      spyOn(page, 'navigate');
+
+      page.leftButtonEvent();
+      expect(page.navigate).toHaveBeenCalledWith('/');
+
+    });
+  });
 });
--- client/src/js/pages/SOSPage.js
+++ client/src/js/pages/SOSPage.js
@@ -1,15 +1,29 @@
 const BasePage = require('watch-framework').BasePage;
 
 class SOSPage extends BasePage {
-    template = require('../../templates/SOSPage.hbs');
 
-    rightButtonEvent() {
-      this.navigate('SOSCall');
+  countdown = 10;
+  template = require('../../templates/SOSPage.hbs');
+
+  pageWillLoad() {
+    this.countdown = 10;
+    setInterval(() => this.timerCallBack(), 1000)
+  }
+
+  rightButtonEvent() {
+    this.navigate('SOSCall');
+  }
+
+  leftButtonEvent() {
+    this.navigate('/');
+  }
+
+  timerCallBack() {
+    this.countdown -= 1;
+    if(this.countdown == 0){
+      this.navigate("SOSCall");
     }
-   
-    leftButtonEvent() {
-      this.navigate('/');
-    }
+  }
 }
 
 module.exports = SOSPage;
